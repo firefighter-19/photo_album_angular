9d3886c342a28d2fbc95261c69c74824e374faf8
sazonov <fromlhz03@gmail.com>
2022-08-28 12:08:50 +0300

    add api GET method test

--- src/app/api/api.service.spec.ts
+++ src/app/api/api.service.spec.ts
@@ -1,16 +1,62 @@
+import { IPhotoAlbum } from './api.types';
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { ApiService } from './api.service';
+import {
+  ApiService,
+  PLACEHOLDER_ALBUM_URL,
+  PLACEHOLDER_PHOTOS_URL,
+} from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
+
+  const mockData: IPhotoAlbum[] = [
+    {
+      id: 1,
+      photos: [
+        {
+          albumId: 1,
+          id: 1,
+          thumbnailUrl: 'url',
+          title: 'title',
+          url: 'url',
+        },
+      ],
+      title: 'Album title',
+      userId: 1,
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Get photos and albums', () => {
+    it('should get from placeholder API data', (done: DoneFn) => {
+      service.getPhotoAlbums().subscribe((value) => {
+        expect(value).toEqual(mockData);
+      });
+
+      const requestAlbums = httpMock.expectOne(PLACEHOLDER_ALBUM_URL);
+      const requestPhotos = httpMock.expectOne(PLACEHOLDER_PHOTOS_URL);
+
+      expect(requestAlbums.request.method).toBe('GET');
+      expect(requestPhotos.request.method).toBe('GET');
+
+      done();
+    });
   });
 });
--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { Observable, zip, map } from 'rxjs';
 import { IAlbum, IPhoto, IPhotoAlbum } from './api.types';
 
-const PLACEHOLDER_ALBUM_URL =
+export const PLACEHOLDER_ALBUM_URL =
   'https://jsonplaceholder.typicode.com/albums?_start=0&_limit=5';
 
-const PLACEHOLDER_PHOTOS_URL =
+export const PLACEHOLDER_PHOTOS_URL =
   'https://jsonplaceholder.typicode.com/photos?_start_0&_limit=60';
 
 @Injectable({
